dc1126906f6e9d6556567aa7e14b973a9c7adc85
Martin Valba <kailing2@live.com>
2020-09-17 19:00:18 +0300

    Update beryllium.sh

--- scripts/beryllium.sh
+++ scripts/beryllium.sh
@@ -3,5 +3,5 @@ git clone https://github.com/PWN-Hunter/BerylliumHunter-T420.git -b Q $SOURCE/de
 echo "Cloning kernel"
 git clone https://github.com/Team-420/android_kernel_xiaomi_sdm845.git $SOURCE/kernel/xiaomi/sdm845
 echo "Cloning vendor"
-gitt clone https://github.com/PWN-Hunter/Beryllium-vendor.git $SOURCE/vendor/xiaomi
+git clone https://github.com/PWN-Hunter/Beryllium-vendor.git $SOURCE/vendor/xiaomi
 echo "DONE"
